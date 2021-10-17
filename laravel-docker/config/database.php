d42a995c341b7b15ba05f5330a15e4e665d22435
ezequielln87 <ezequielln87@gmail.com>
2021-10-17 00:33:28 -0300

    adicionado crud laravel

--- laravel-docker/config/database.php
+++ laravel-docker/config/database.php
@@ -46,15 +46,15 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '172.28.151.18'),
+            'host' => env('DB_HOST', '192.168.15.105'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'portfolio'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'satelite'),
+            'username' => env('DB_USERNAME', 'ezequiel'),
+            'password' => env('DB_PASSWORD', 'dev123'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
+            'collation' => 'utf8mb4_general_ci',
+            'prefix' => 'por',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'portfolio'), '_').'_database_'),
         ],
 
         'default' => [
